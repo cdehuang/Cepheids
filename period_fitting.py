cb4660b36516d29d42490cae90c00482fb50baf2
cdehuang <cdehuang@users.noreply.github.com>
2015-02-27 08:33:10 -0500

    Update period_fitting.py

--- period_fitting.py
+++ period_fitting.py
@@ -11,6 +11,7 @@ from PyAstronomy.pyTiming import pyPDM
 import cepheid_plotting
 from scipy import interpolate
 from scipy.interpolate import UnivariateSpline
+from scipy import optimize
 
 #   Fits period to data using phase dispersion minimization, and plots the theta values. Good for data that is sparse and has a lot baseline. This is also good when you have a decent initial guess to the period of the Cepheid (within a 4-day range)
 #   Need to get rid of the odd phase shift you have.
@@ -41,15 +42,15 @@ def extract_median(t, y):
     return binned_t, binned_mag
 
 
-def PDM_precise(t, y, period, name='none', precision=0.001):
+def PDM_precise(t, y, period, name='none', precision=0.01, nbins=15, covers=3):
 
     minperiod = period - 2.0
     maxperiod = period + 2.0
     S = pyPDM.Scanner(minVal=minperiod, maxVal=maxperiod, dVal=precision, mode="period")
     P = pyPDM.PyPDM(t, y)
     print "Trying fit..."
-    f1, t1 = P.pdmEquiBinCover(10, 3, S)
-    f2, t2 = P.pdmEquiBin(10, S)
+    f1, t1 = P.pdmEquiBinCover(nbins, 3, S)
+    f2, t2 = P.pdmEquiBin(nbins, S)
 
     best_fit_idx = t1.argmin()
     revised_period = f1[best_fit_idx]
@@ -85,12 +86,119 @@ def PDM_precise(t, y, period, name='none', precision=0.001):
     plt.gca().invert_yaxis()
     plt.ylabel("V-band Magnitude")
     plt.xlabel("Phase")
+    """
     if name!='none':
         plt.savefig("../output/{}_pdm_fitting.png".format(name))
         plt.title("Result of PDM analysis for {}".format(name))
     else:
         plt.title("Result of PDM analysis")
+    """
     plt.show()
+    return revised_period, t1[best_fit_idx]
+
+def PDM_equibin(t, y, period, name='none', precision=0.001, nbins=15):
+    
+    minperiod = period - 2.0
+    maxperiod = period + 2.0
+    S = pyPDM.Scanner(minVal=minperiod, maxVal=maxperiod, dVal=precision, mode="period")
+    P = pyPDM.PyPDM(t, y)
+    print "Trying fit..."
+    f2, t2 = P.pdmEquiBin(nbins, S)
+    
+    best_fit_idx = t2.argmin()
+    revised_period = f2[best_fit_idx]
+    print "Best fit period:", revised_period
+    
+    phase_shift = y[0]/revised_period - y[0]/period
+    
+    pp, yy = cepheid_plotting.plot_array(t, y, revised_period)
+    pp2, yy2 = cepheid_plotting.plot_array(t, y, period)
+    
+    #    pp_ind = np.argsort(pp)
+    #    pp = pp[pp_ind]
+    #    yy = yy[pp_ind]
+    
+    #    deltaphases = np.linspace(0, 1, 1001)
+    #    s = UnivariateSpline(pp2, yy2)
+    #    phasemags = s(deltaphases)
+    #    phaseshift = deltaphases[np.argmax(phasemags)]
+    #    print phaseshift
+    #    pp = pp - phaseshift
+    
+    plt.clf()
+    plt.subplot(2, 1, 1)
+    plt.xlabel("Frequency")
+    plt.ylabel("Theta")
+    plt.plot(f2, t2, 'gp-')
+    #plt.legend(["pdmEquiBinCover", "pdmEquiBin"])
+    plt.text(minperiod, 1.1, "best fit period = {} days".format(revised_period), fontsize=14)
+    plt.subplot(2, 1, 2)
+    plt.scatter(pp, yy, alpha=0.7, color='red')
+    #plt.scatter(pp2, yy2, alpha=0.7, color='grey')
+    plt.gca().invert_yaxis()
+    plt.ylabel("V-band Magnitude")
+    plt.xlabel("Phase")
+    """
+    if name!='none':
+        plt.savefig("../output/{}_pdm_fitting.png".format(name))
+        plt.title("Result of PDM analysis for {}".format(name))
+    else:
+        plt.title("Result of PDM analysis")
+    """
+    plt.show()
+
+def PDM_equibincover(t, y, period, name='none', precision=0.001, nbins=15, covers=3):
+    
+    minperiod = period - 2.0
+    maxperiod = period + 2.0
+    S = pyPDM.Scanner(minVal=minperiod, maxVal=maxperiod, dVal=precision, mode="period")
+    P = pyPDM.PyPDM(t, y)
+    print "Trying fit..."
+    f1, t1 = P.pdmEquiBinCover(nbins, covers, S)
+    
+    best_fit_idx = t1.argmin()
+    revised_period = f1[best_fit_idx]
+    print "Best fit period:", revised_period
+    
+    phase_shift = y[0]/revised_period - y[0]/period
+    
+    pp, yy = cepheid_plotting.plot_array(t, y, revised_period)
+    pp2, yy2 = cepheid_plotting.plot_array(t, y, period)
+    
+    #    pp_ind = np.argsort(pp)
+    #    pp = pp[pp_ind]
+    #    yy = yy[pp_ind]
+    
+    #    deltaphases = np.linspace(0, 1, 1001)
+    #    s = UnivariateSpline(pp2, yy2)
+    #    phasemags = s(deltaphases)
+    #    phaseshift = deltaphases[np.argmax(phasemags)]
+    #    print phaseshift
+    #    pp = pp - phaseshift
+    
+    plt.clf()
+    plt.subplot(2, 1, 1)
+    plt.xlabel("Frequency")
+    plt.ylabel("Theta")
+    plt.plot(f1, t1, 'bp-')
+    plt.legend(["pdmEquiBinCover", "pdmEquiBin"])
+    plt.text(minperiod, 1.1, "best fit period = {} days".format(revised_period), fontsize=14)
+    plt.subplot(2, 1, 2)
+    plt.scatter(pp, yy, alpha=0.7, color='red')
+    #plt.scatter(pp2, yy2, alpha=0.7, color='grey')
+    plt.gca().invert_yaxis()
+    plt.ylabel("V-band Magnitude")
+    plt.xlabel("Phase")
+    """
+    if name!='none':
+        plt.savefig("../output/{}_pdm_fitting.png".format(name))
+        plt.title("Result of PDM analysis for {}".format(name))
+    else:
+       plt.title("Result of PDM analysis")
+    """
+    plt.show()
+
+
 
 #   Input parameter depends on if the period is changing or not.
 #   If the period is not changing, then use all of the data or the binned data to fit
